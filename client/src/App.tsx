ca66c8f1b3993d247a9239c893a6a829a7d4d16d
Jordan Hill <74479504+Jordanhill90@users.noreply.github.com>
2024-01-05 17:40:56 -0500

    first commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,110 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const API_URL = "http://localhost:8080";
+const LOGIN_URL = "http://localhost:8080/login"
 
 function App() {
-  const [data, setData] = useState<string>();
+  const [data, setData] = useState<string>("");
+  const [token, setToken] = useState<string | null>(null)
+
+  const login = async () => {
+   try {
+    const response = await fetch(LOGIN_URL, {
+        method: "POST",
+        body: JSON.stringify({ username: "George"}), // sample username since login is simulated
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+    });
+    const { accessToken } = await response.json()
+    setToken(accessToken)
+   } catch (error) {
+    console.error("Error during login", error.message)
+   }
+    
+  }
+
+  const getData = useCallback(async (fetchToken: string | null) => {
+    try {
+        if(!fetchToken) {
+            return
+        }
+        
+        const response = await fetch(API_URL, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${fetchToken}`
+            }
+        } );
+
+        if(!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+
+        const { data } = await response.json();
+        setData(data);
+    } catch (error) {
+        console.error("Error fetching", error.message)
+    }
+  }, [])
 
   useEffect(() => {
-    getData();
-  }, []);
+    login()
+  }, [])
 
-  const getData = async () => {
-    const response = await fetch(API_URL);
-    const { data } = await response.json();
-    setData(data);
-  };
+  useEffect(() => {
+    if(token) {
+        getData(token);
+    }
+  }, [token, getData]);
+
+  
+
+  
 
   const updateData = async () => {
+
+    if(!token) {
+        return
+    }
     await fetch(API_URL, {
       method: "POST",
       body: JSON.stringify({ data }),
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
+       Authorization: `Bearer ${token}`
       },
     });
 
-    await getData();
+    await getData(token);
   };
 
   const verifyData = async () => {
-    throw new Error("Not implemented");
+    try {
+        const response = await fetch(API_URL, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        } );
+
+        if(!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`)
+        }
+
+        const { data: serverData } = await response.json();
+
+        if(serverData === data) {
+            alert('Data is verified')
+        } else {
+            alert('Data is not verified and will be reverted')
+            setData(serverData)
+        }
+    } catch (error) {
+        console.log("Error verifying", error.message)
+    }
   };
 
   return (
--- server/app.ts
+++ server/app.ts
@@ -1,23 +1,54 @@
 import express from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+
+import type { Request, Response, NextFunction } from "express";
 
 const PORT = 8080;
 const app = express();
 const database = { data: "Hello World" };
+
+// Secret stored here instead of in .env for demonstration
+const ACCESS_TOKEN_SECRET =
+  "56f7b247ebb63451cf3f75abbe94114c502517e87ff69a5862b93e739ee5abdd640d680b8d451a65ccd41c14c8f83b67845184a834055b77c645597c658e7f11";
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
 
-app.get("/", (req, res) => {
+app.get("/", authenticateToken, (_req, res) => {
   res.json(database);
 });
 
-app.post("/", (req, res) => {
-  database.data = req.body.data;
+app.post("/", authenticateToken, (req, res) => {
+  const { data } = req.body;
+  database.data = data;
   res.sendStatus(200);
 });
+
+// Sample route to simulate a successful login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+// Middleware to verify jwt
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.body.user = user;
+    next();
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
